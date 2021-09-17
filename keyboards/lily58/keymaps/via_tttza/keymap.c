d2ed3071d300132f4eb1c9e74400dacd3a6ef8dc
tttza <25410554+tttza@users.noreply.github.com>
2021-09-17 12:34:32 +0900

    Add function keys to 4th row.

--- keyboards/lily58/keymaps/via_tttza/keymap.c
+++ keyboards/lily58/keymaps/via_tttza/keymap.c
@@ -102,7 +102,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_RAISE] = LAYOUT( \
-  _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______, \
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS, \
   KC_F1,  KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,                       KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_HOME, KC_END,  \
   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,   KC_PSCR, KC_INS,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_PGUP, KC_PGDN, \
@@ -366,6 +366,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case RAISE:
         return process_raise(keycode, record);
     default:
+      // NOTE: is redundant?
       if (lower_pressed) { lower_cmb_pressed = true; }
       if (raise_pressed) { raise_cmb_pressed = true; }
       if (user_config.jis){
